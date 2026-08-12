120c4292d71b60790ef69c507c3053caf3481dc4
n30nex <blushellneon@outlook.com>
2026-08-12 04:45:48 -0400

    feat(ui): add triple-press home and clear (#7)

--- examples/companion_radio/ui-new/UITask.cpp
+++ examples/companion_radio/ui-new/UITask.cpp
@@ -545,6 +545,14 @@ class HomeScreen : public UIScreen {
 
 public:
 #ifdef NEONPOCKET_UI
+  void neonGoHome() {
+    _page = HomePage::FIRST;
+    _shutdown_init = false;
+    _transition_dir = -1;
+    _transition_started = 0;
+    _transition_pending = true;
+  }
+
   void neonRequestShutdown() {
     _page = HomePage::SHUTDOWN;
     _shutdown_init = true;
@@ -1054,7 +1062,7 @@ public:
     display.setColor(NEON_BLUE);
     display.setCursor(4, 113);
     display.print(page_has_more ? "CLICK MORE" : "CLICK NEXT");
-    display.drawTextRightAlign(display.width() - 4, 113, "2X CLEAR ALL");
+    display.drawTextRightAlign(display.width() - 4, 113, "3X CLEAR ALL");
     return 1000;
 #else
     char tmp[16];
@@ -1778,8 +1786,17 @@ char UITask::handleDoubleClick(char c) {
 
 char UITask::handleTripleClick(char c) {
   MESH_DEBUG_PRINTLN("UITask: triple click triggered");
+#ifdef NEONPOCKET_UI
+  _power_confirm_until = 0;
+  c = checkDisplayOn(KEY_SELECT);
+  if (c == 0) return 0;
+  if (curr == msg_preview) curr->handleInput(KEY_ENTER);
+  static_cast<HomeScreen*>(home)->neonGoHome();
+  gotoHomeScreen();
+#else
   checkDisplayOn(c);
   toggleBuzzer();
+#endif
   c = 0;
   return c;
 }
